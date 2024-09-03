20ef9c34d184c12c20608edfdcbf67de43e9d044
dominicmagenta <dominic@7fivefive.com>
2024-09-03 12:05:46 +0100

    Correct package name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'fgiraffe@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'packages': ['pp_parse'],
     'scripts': [],
     'name': 'pp_parse'
 }
